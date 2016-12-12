d496ce17a5fc5f1f98cbf697558ce179e3343348
James Wen <jrw2175@columbia.edu>
2016-12-12 11:50:50 -0500

    Add debug logging to fixture
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- cf_spec/fixtures/asp_web_app/Startup.cs
+++ cf_spec/fixtures/asp_web_app/Startup.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace HelloWeb
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Debug);
+
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello World!");
